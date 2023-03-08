a7b7fc5853a1a1dadd84ef99f49a71328854a5bc
Aniketjiotode <aniketjiotode07@gmail.com>
2023-03-08 17:40:20 +0530

    Added: UC_1 Changes

---
+++ ProductReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    internal class ProductReview
+    {
+        public int ProductId;
+        public int UserId;
+        public double Rating;
+        public string Review;
+        public bool isLike;
+
+    }
+}
